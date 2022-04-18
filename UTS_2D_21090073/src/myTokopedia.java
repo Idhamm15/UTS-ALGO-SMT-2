68a9d6a0079907c6672b54340e38802445f83269
Idhamm15 <idhamtamvanz123@gmail.com>
2022-04-18 18:47:12 +0700

    uts 2D

---
+++ UTS_2D_21090073/src/myTokopedia.java
@@ -0,0 +1,10 @@
+import Model.Tokopedia;
+import Model.keranjang;
+
+public class myTokopedia {
+    public static void main(String[] args) {
+        keranjang saya = new keranjang();
+        Tokopedia [] Produk = new Tokopedia[5];
+        Produk[0] = new Tokopedia("")
+    }
+}
